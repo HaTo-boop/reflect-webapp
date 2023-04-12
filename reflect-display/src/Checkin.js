50e1de76790f8960fc5aa3f5eda04c4803aba043
udao20 <uyendao20@Uyens-MacBook-Pro.local>
2023-04-12 01:20:25 -0700

    display the emoticons for checkin

--- reflect-display/src/Checkin.js
+++ reflect-display/src/Checkin.js
@@ -1,5 +1,6 @@
 import { CardButton } from "./Buttons"
 import './App.css';
+/*import card from './card.png';*/
 
 import { Link } from "react-router-dom"
 
@@ -7,11 +8,9 @@ import { Link } from "react-router-dom"
 
 export function CHECKIN(props) {
     return (
-        <div id='daily-checkin' className='glassmorphism vert-flex'>
+        <div>
             <Greetings/>
-            <div className="checkin-card">
-                <img src="public\card.png" alt="Happy"/>
-            </div>
+            <Cards/>
             <Skip/>
         </div>
     )
@@ -21,6 +20,24 @@ function Greetings(props) {
     return (
         <div className="affirmation glassmorphism hort-flex">
             hello avery, how are you doing, really?
+        </div>
+    )
+}
+
+function Cards(props) {
+    return (
+        <div className="checkin-row">
+            <div className="checkin-card-1">
+                <img src="happy.png" />
+            </div>
+
+            <div className="checkin-card-2">
+                <img src="inspired.png" />
+            </div>
+
+            <div className="checkin-card-3">
+                <img src="focused.png" />
+            </div>
         </div>
     )
 }
