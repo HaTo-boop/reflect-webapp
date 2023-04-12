c4fa6dcf13e9cf9d97d9adb109f01fce67b740b2
udao20 <uyendao20@Uyens-MacBook-Pro.local>
2023-04-12 01:24:10 -0700

    emoticons are now buttons

--- reflect-display/src/Checkin.js
+++ reflect-display/src/Checkin.js
@@ -1,6 +1,5 @@
 import { CardButton } from "./Buttons"
 import './App.css';
-/*import card from './card.png';*/
 
 import { Link } from "react-router-dom"
 
@@ -27,17 +26,17 @@ function Greetings(props) {
 function Cards(props) {
     return (
         <div className="checkin-row">
-            <div className="checkin-card-1">
+            <button className="checkin-card-1">
                 <img src="happy.png" />
-            </div>
+            </button>
 
-            <div className="checkin-card-2">
+            <button className="checkin-card-2">
                 <img src="inspired.png" />
-            </div>
+            </button>
 
-            <div className="checkin-card-3">
+            <button className="checkin-card-3">
                 <img src="focused.png" />
-            </div>
+            </button>
         </div>
     )
 }
