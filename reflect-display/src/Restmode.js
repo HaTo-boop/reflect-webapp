976325e4ad1db07a3b18b0d765abc36ce3ada085
udao20 <uyendao20@Uyens-MacBook-Pro.local>
2023-04-20 17:50:51 -0700

    added weather API

--- reflect-display/src/Restmode.js
+++ reflect-display/src/Restmode.js
@@ -2,6 +2,7 @@ import './App.css';
 
 import { Link } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export function RESTMODE(props) {
     return (
@@ -16,6 +17,21 @@ export function RESTMODE(props) {
             <Settings/>
         </div>
     )
+}
+
+export function RestWidget(props) {
+  return (
+      <div id='rest-widget' className='widget glassmorphism vert-flex'>
+          {/* <Clock/> */}
+          <Time />
+          <Weather />
+          <div id='theme'>
+              monthly theme
+              {/* <CardButton imgSrc="/public/balance_theme.png" content="balance"/> */}
+              <div style={{ margin:"10px auto" }}><b style={{ fontSize:"35px" }}>balance</b></div>
+          </div>
+      </div>
+  )
 }
 
 // Components appeared in restmode of the mirror
@@ -46,20 +62,7 @@ export function Affirmations(props) {
     )
 }
 
-export function RestWidget(props) {
-    return (
-        <div id='rest-widget' className='widget glassmorphism vert-flex'>
-            {/* <Clock/> */}
-            <Time />
-            <Weather/>
-            <div id='theme'>
-                monthly theme
-                {/* <CardButton imgSrc="/public/balance_theme.png" content="balance"/> */}
-                <div style={{ margin:"10px auto" }}><b style={{ fontSize:"35px" }}>balance</b></div>
-            </div>
-        </div>
-    )
-}
+
 
 function Time() {
     const [time, setTime] = useState(new Date());
@@ -90,54 +93,33 @@ function Time() {
       </div>
     );
   }
-  
-  function Weather() {
-    // const [temperature, setTemperature] = useState(null);
-    // componentDidMount() {
-    //     const API_KEY = 'a0a00327131618a6c859378f3f0dfeb2';
-    //     const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${API_KEY}`;
-    // }
-  
-    // useEffect(() => {
-    //   const URL = `https://api.openweathermap.org/data/`;
-  
-    //   fetch(URL)
-    //     .then(response => response.json())
-    //     .then(data => {
-    //       const tempF = data.hourly.temperature_2m[0].value * 1.8 + 32; // convert to Fahrenheit
-    //       setTemperature(tempF);
-    //     })
-    //     .catch(error => {
-    //       console.error('Error fetching temperature:', error);
-    //     });
-    // }, []);
-  
-    return (
-      <div>
-        <h1>weather</h1>
-        {/* {temperature !== null && <p>{temperature}°F</p>} */}
-      </div>
-    );
-}
 
-/*
-function Clock(props) {
-//API
-return(
-    <div className="clock">
-        <p id="time"> 10:00 </p> 
-        <p id="date"> friday, march 2 </p>
-    </div>
-)
-}
+// Get the weather API
+function Weather() {
+  const [weatherData, setWeatherData] = useState(null);
+  const API_KEY = 'bf9dd0c1c9eaa101b363ce544816179b';
+  const CITY = 'Seattle';
 
-function Weather(props) {
-    //API
-    return (
-        <div id="weather">
-            Weather
+  useEffect(() => {
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${API_KEY}&units=imperial`)
+      .then((response) => {
+        setWeatherData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [API_KEY, CITY]);
+
+  return (
+    <div>
+      {weatherData && (
+        <div>
+          <h2>{weatherData.name}</h2>
+          <p>{weatherData.main.temp} °F</p>
+          {/* <p>Conditions: {weatherData.weather[0].description}</p> */}
         </div>
-    )
+      )}
+    </div>
+  );
 }
-
-*/
