60cdfdb1b2c51b89e1abe93b70edb362be8cf57a
HaTo-boop <nhatha.to.2001@gmail.com>
2023-05-12 07:57:26 -0700

    Added static data (no fetching - simplify use of React hooks). ERROR: Still have invalid hook error because of PromptContentTherapy

--- reflect-display/src/ReflectionSessions/reflection.js
+++ reflect-display/src/ReflectionSessions/reflection.js
@@ -28,44 +28,77 @@ export function DisplayFeature(){
 }
 
 export function ToRandRef() {
-    const chosenTheme = 'gratitude';
-    const [refs, setRefs] = useState(() => {return []});
+    // const chosenTheme = 'gratitude';
+    // const [refs, setRefs] = useState(() => {return []});
 
-    console.log('ToRandRef renders');
-    // hook only run when there is a change in refs
-    useEffect(() => {
-        fetch('/data/reflect-reflections.json')
-            .then(response => response.json())
-            .then(json => {
-                // Get array of reflection questions based on the theme
-                let refArray = json[chosenTheme];
-                console.log(refArray);
-                // Generate 3 random reflection questions (with individual replacement)
-                let randInds = [];  //to handle when randomly generated index are repeated among generations
-                let randRefs = [];
+    // console.log('ToRandRef renders');
+    // // hook only run when there is a change in refs
+    // useEffect(() => {
+    //     fetch('/data/reflect-reflections.json')
+    //         .then(response => response.json())
+    //         .then(json => {
+    //             // Get array of reflection questions based on the theme
+    //             let refArray = json[chosenTheme];
+    //             console.log(refArray);
+    //             // Generate 3 random reflection questions (with individual replacement)
+    //             let randInds = [];  //to handle when randomly generated index are repeated among generations
+    //             let randRefs = [];
 
-                for (let i = 0; i < 3; i++) {
-                    let index = getRandomIndex(refArray.length);
-                    // if the index generated duplicates with one of those already in the randInds, run generator again
-                    // while (randInds.includes(index)) {
-                    //     index = getRandomIndex(refArray.length);
-                    // }
-                    console.log(refArray[index]);
-                    randInds.push(index);
-                    randRefs.push(refArray[index]);
-                }
-                console.log(randInds);
+    //             for (let i = 0; i < 3; i++) {
+    //                 let index = getRandomIndex(refArray.length);
+    //                 // if the index generated duplicates with one of those already in the randInds, run generator again
+    //                 // while (randInds.includes(index)) {
+    //                 //     index = getRandomIndex(refArray.length);
+    //                 // }
+    //                 console.log(refArray[index]);
+    //                 randInds.push(index);
+    //                 randRefs.push(refArray[index]);
+    //             }
+    //             console.log(randInds);
                 
-                // Update refs
-                setRefs(randRefs);
+    //             // Update refs
+    //             setRefs(randRefs);
                 
-            })
-    }, [refs]);
+    //         })
+    // }, [refs]);
 
+    // Static data: Random 3-4 quetions from 'general' in /public/reflect-reflection.json
+    const generalRefs = [
+        "what are your lows?",
+        "what are your highs?",
+        "did you set any goals recently?",
+        "what is a recent lesson you've learned?",
+        "what have you recently accomplished?",
+        "were there any challenges you overcome today (or recently)?",
+        "what are you proud of yourself for?",
+        "what aspects of yourself are you grateful for today?",
+        "how do you show kindness to yourself?",
+        "what do you look forward to tomorrow?",
+        "were there anything that didn't meet your expectations recently?",
+        "what personal strengths have you used recently?",
+        "what sparked joy today?"
+    ];
 
+    // Get random 3 refs
+    let randInds = [];
+    let randRefs = [];
+    for (let i = 0; i < 3; i++) {
+        let index = getRandomIndex(generalRefs.length);
+        // if the index generated duplicates with one of those already in the randInds, run generator again
+        while (randInds.includes(index)) {
+            index = getRandomIndex(generalRefs.length);
+        }
+
+        randInds.push(index);
+        randRefs.push(generalRefs[index]);
+    }
+    console.log(randInds);
+    console.log(randRefs);
+
+    // IMPRPOVE: May need to define their own PromptContent. Using PromptContentTherapy (including TherapyEnd) in TherapySession/Therapy.js for now
     return (
         <div>
-            {PromptContentTherapy(refs)}
+            {PromptContentTherapy(randRefs)}
         </div>
     )
 
--- reflect-display/src/TherapySessions/Therapy.js
+++ reflect-display/src/TherapySessions/Therapy.js
@@ -92,32 +92,52 @@ export function PromptContentTherapy(promptsArray) {
 
 // Handle continue button click in TherapyIntro.js . Displaying info of random
 export function ToRandSes() {
-    const [sesId, setSesId] = useState(0);  //can be omit since only need to display prompt - will do latre bc low in priority
-    const [prompts, setPrompts] = useState(() => {return []});
+    // const [sesId, setSesId] = useState(0);  //can be omit since only need to display prompt - will do latre bc low in priority
+    // const [prompts, setPrompts] = useState(() => {console.log('Inside prompt state');return []});
 
-    console.log('ToRandSes renders');
-    // hook only run when there is a change in sesId
-    useEffect(() => {
-        fetch('/data/reflect-exp-therapy.json')
-            .then(response => response.json())
-            .then(json => {
-                // generate a random index to choose a random session from list of sessions in josn file
-                setSesId(getRandomIndex(json.length));
-                // PRINT to inspect
-                console.log("length: ", json.length);
-                console.log("Chosen sesId: ", sesId);
+    // console.log('ToRandSes renders');
+    // // hook only run when there is a change in sesId
+    // useEffect(() => {
+    //     fetch('/data/reflect-exp-therapy.json')
+    //         .then(response => response.json())
+    //         .then(json => {
+    //             // generate a random index to choose a random session from list of sessions in josn file
+    //             setSesId(getRandomIndex(json.length));
+    //             // PRINT to inspect
+    //             console.log("length: ", json.length);
+    //             console.log("Chosen sesId: ", sesId);
 
-                // Declare var to store chosen session data
-                let chosen = json[sesId];
+    //             // Declare var to store chosen session data
+    //             let chosen = json[sesId];
                 
-                console.log("Chosen session's object: ", chosen);
+    //             console.log("Chosen session's object: ", chosen);
 
-                // update prompts to be the array of prompts for the session
-                setPrompts(chosen.prompts);
+    //             // update prompts to be the array of prompts for the session
+    //             setPrompts(chosen.prompts);
                 
-            })
-    }, [sesId]);
+    //         })
+    // }, [sesId]);
 
+    // Static data: Prompt content of Session 1 from /public/reflect-exp-therapy.json
+    const prompts = [
+        "it may be uncomfortable, but if you are willing, please wear garments that will allow you to have a clear reflection of you",
+        "alright, are you ready? ",
+        "let's start by relaxing your body",
+        "unclench your jaw",
+        "drop your shoulders, shake your hands if you'd like",
+        "breath in, breath out",
+        "remember, you're navigating this at your own pace",
+        "today, you will be focusing on your individual body parts and describing it using neutral language",
+        "neutral language as in, terms that simply describe your body's characteristics without making any value judgments or comparisons to others\n\nuse descriptions that would help someone build a model of you",
+        "let's take a look at your reflection",
+        "starting with your head, use neutral language to describe the shapes and colors of your head\n\nfor instance, what is your hair color? what shape are your lips and eyes?",
+        "now, use neutral language to describe the shapes and color of your torso\n\nfor instance, what shape best describes your torso?",
+        "now, use neutral language to describe the shapes and colors of the bottom section of your body\n\nfor instance, what shape best describes your legs?",
+        "if there were areas that you weren't as ready to talk about today, it's alright, come back to them when you're ready",
+        "reflection time (please answer as your comfort allows)!\n\nwere there any areas you spent more time on and why?",
+        "how are you feeling now that you've practiced describing your body using neutral language?",
+        "well done\n\nbeing kind to yourself may not be easy but you're learning how to embrace yourself step-by-step!"
+    ]
 
     return (
         <div>
